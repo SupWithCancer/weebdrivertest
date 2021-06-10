ce471d3a920c14ff535fea0e063364bfc0116ede
supwithcancer <54910786+SupWithCancer@users.noreply.github.com>
2021-06-10 17:29:05 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+search_request = 'ноутбук'
+url = 'https://www.citrus.ua'
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+browser.implicitly_wait(10)
+
+browser.get(url)
+browser.implicitly_wait(10)
+
+browser.find_element_by_css_selector('[name="search"]').send_keys(search_request)
+browser.find_element_by_css_selector('[name="search"]').send_keys(Keys.ENTER)
+browser.implicitly_wait(10)
+
+elements = browser.find_elements_by_css_selector('[class="result-title"]')
+
+actualResult = elements[0].text
+
+expectedResult = "Результаты поиска: ноутбук"
+
+assert actualResult == expectedResult
+
+browser.close()

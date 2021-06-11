ded89597974a12f7f80bc0f7b44cabf41de3d62e
supwithcancer <54910786+SupWithCancer@users.noreply.github.com>
2021-06-11 18:46:12 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,17 +5,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 search_request = 'ноутбук'
-url = 'https://www.citrus.ua'
+url = 'https://www.citrus.ua/'
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.implicitly_wait(10)
 
+
+
 browser.get(url)
-browser.implicitly_wait(10)
 
 browser.find_element_by_css_selector('[name="search"]').send_keys(search_request)
 browser.find_element_by_css_selector('[name="search"]').send_keys(Keys.ENTER)
-browser.implicitly_wait(10)
+
 
 actualResult = browser.find_element_by_css_selector('[class="result-title"]').text
 
